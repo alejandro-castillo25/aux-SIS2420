3d68c60ed5abdeeb45f3f9c0f115556c5925b258
Alejandro Castillo <alejandrocastillo252006@gmail.com>
2025-06-01 16:55:23 -0400

    - Add Gato class - Fix some formatting

--- oop2/main.py
+++ oop2/main.py
@@ -1,17 +1,21 @@
 
-#? Variable names are in spanish, because... it says in the problem
-#? Igual es buena practica hacerlas en inglés
+# ? Variable names are in spanish, because... it says in the problem
+# ? Igual es buena practica hacerlas en inglés
 
 class Animal:
     def __init__(self, nombre: str, edad: int) -> None:
         self.nombre = nombre
         self.edad = edad
-    
+
     def emitir_sonido(self) -> None:
         print("Este animal hace un sonido :]")
 
 
-
 class Perro(Animal):
     def emitir_sonido(self) -> None:
-            print("Guau Guau!")
+        print("Guau Guau!")
+
+
+class Gato(Animal):
+    def emitir_sonido(self) -> None:
+        print("Miau!")
