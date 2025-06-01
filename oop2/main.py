f70fddec7ace8059ec9ec5cf2c5606c2d78d1d5a
Alejandro Castillo <alejandrocastillo252006@gmail.com>
2025-06-01 16:52:05 -0400

    - Add Animal class

--- oop2/main.py
+++ oop2/main.py
@@ -2,6 +2,13 @@
 #? Variable names are in spanish, because... it says in the problem
 #? Igual es buena practica hacerlas en inglés
 
-# TODO
+class Animal:
+    def __init__(self, nombre: str, edad: int) -> None:
+        self.nombre = nombre
+        self.edad = edad
+    
+    def emitir_sonido(self) -> None:
+        print("Este animal hace un sonido :]")
+
 
 
